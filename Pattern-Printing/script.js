450ab2d478f465ba0ff97faa3055ec44faa70210
Aditya Kumar <adityadreamer9060@gmail.com>
2025-03-07 16:54:40 +0530

    array n-loop string funcation complete

---
+++ Pattern-Printing/script.js
@@ -0,0 +1,369 @@
+console.log("Pattern - Printing");
+
+
+// for ( let j=1; j<=5; j++){
+//     let bag = "" ;
+
+// for ( let i=1; i<=5; i++){
+//     bag += i + " "
+// }
+
+
+// console.log(bag);
+// }
+// let bag = ""
+// for ( j=1; j<=5; j++){
+//     for ( let i=1; i<=5; i++){
+//         bag += i
+        
+//     }
+//     console.log(bag);
+    
+// }
+
+// let bag = " "
+// for ( let j=1; j<=5; j++){
+//     for ( let i=1; i<=5; i++){
+//         bag += i
+//     }
+//     console.log(bag);
+// }
+
+// for ( let i=1; i<=5; i++){
+//     let bag = " ";
+
+//     for ( let i=1; i<=5; i++){
+//         bag += "$" + " "
+//     }
+//     console.log(bag);
+// }
+
+// for ( let j=1; j<=5; j++){
+//     let bag = " ";
+
+//     for ( let i=1; i<=j; i++){
+//         bag += "$" + " "
+//     }
+//     console.log(bag);
+// }
+// for ( let j=1; j<=6; j++){
+//     let bag = ""
+//     for ( let i=1; i<=j; i++){
+//         bag += "*" + " "
+//     }
+//     console.log(bag);
+    
+// }
+
+// for ( let j=7; j>=0; j--){
+//     bag = " "
+//     for ( let i=1; i<=j; i++){
+//         bag += '*' + " "
+//     }
+//     console.log(bag);
+    
+// }
+
+
+// for ( let i=1; i<=4; i++){
+//     bag = ""
+//     for ( let j=1; j<=i; j++){
+//         bag += "*" + " "
+//     }
+//     console.log(bag);
+    
+// }
+// for ( let i=3; i>=0; i++){
+//     bag = ""
+//     for ( let j=1; j<=3; j++){
+//         bag += "*" + " "
+//     }
+//     console.log(bag);
+    
+// }
+
+// for ( let j=1; j<=5; j++){
+//     bag = " "
+//     for ( let i=1; i<=j; i++){
+//         bag += '*' + " "
+//     }
+//     console.log(bag);
+    
+// }
+// for ( let j=4; j>=0; j--){
+//     bag = " "
+//     for ( let i=1; i<=j; i++){
+//         bag += '*' + " "
+//     }
+//     console.log(bag);
+    
+// }
+// for ( let j=4; j>=0; j--){
+//     bag = ""
+//     for ( let i=i; i<=j; i++){
+//         bag += "*" + " "
+//     }
+//     console.log(bag);
+    
+// }
+
+// for(var i = 1; i<=10; i++)
+//     {
+//       var bag="";
+//       for(var j=1; j<=10; j++)
+//       {
+//         if(i==1 || i==10)
+//         {
+//           bag = bag + "*";
+//         }
+//         else
+//         {
+//           if(j==1 || j==10 )
+//           {
+//             bag = bag+"*";
+//           }
+//           else
+//           {
+//             bag = bag+" ";
+//           }
+//         }
+    
+//       }
+//       console.log(bag);
+//     }
+
+
+// let a =6 ;
+// let count = 0
+// for (let i=0; i<=a; i++ ){
+//   if ( a % i == 0){
+//     count ++
+//   }
+// }
+// console.log(count);
+// if ( count === a){
+//   console.log('Its Prime');
+  
+// }else{
+//   console.log('Its not prime');
+  
+// }
+
+
+// let a = 10;
+
+// let count = 0;
+
+// for ( let i=0; i<=a; i++){
+//   if ( a % i == 0){
+//     count++
+//   }
+  
+// }
+// console.log(count);
+// if ( count === a ){
+//   console.log('Its Prime');
+  
+// }
+// else {
+//   console.log("Its Not Prime");
+  
+// }
+
+// let a = 16;
+
+// let count = 0
+
+// for ( let i=0; i<=a; i++){
+//   if ( a % i == 0){
+
+//   }
+//   count++
+  
+// }
+// console.log(count);
+//  if ( count == a){
+//   console.log('Its Prime');
+  
+//  }
+//  else {
+//   console.log('Not Prime');
+  
+//  }
+
+
+// let a = 4  ;
+
+// let count = 0;
+
+// for ( let i=0; i<=a; i++){
+//   if ( a % i == 0) {
+
+//   }
+//   count ++
+// }
+// console.log(count);
+
+// if ( a === count ){
+//   console.log('Prime');
+  
+// }else {
+//   console.log('Not Prime');
+  
+// }
+
+// let a = 4;
+
+// let count = 0 ;
+
+// for ( let i=0; i<=a; i++){
+//   if ( a % i == 0){
+
+//   }
+//   count ++
+// }
+// console.log(count);
+
+
+// let a = 4;
+// let count = 0;
+
+// for (let i = 1; i <= a; i++) {
+//   if (a % i === 0) {
+//     count++;
+//   }
+// }
+
+// console.log(count);
+
+// if (count === 2) {
+//   console.log('Prime');
+// } else {
+//   console.log('Not Prime');
+// }
+
+
+// let a = 3 ; 
+
+// let count = 0;
+
+// for ( let i=1; i<=a; i++){
+//   if ( a % i === 0){
+//     count ++
+
+//   }
+// }
+// console.log(count);
+// if ( count === 2){
+//   console.log('Prime');
+  
+// }else {
+//   console.log('Not Prime');
+  
+// }
+
+
+// let a = [1,2,3,4,5];
+
+// let maxNum = Math.max(...a)
+
+// console.log(maxNum);
+
+
+// let a = [ 1,2,3,4,5];
+// let minnum = Math.min(...a)
+
+// console.log(minnum);
+
+
+// let a = [12, 45 , 65 ];
+// let maxNum = Math.max(...a)
+
+// console.log(maxNum);
+
+
+
+// for ( let i=1; i<=5; i++){
+//   let bag = " "
+//   bag += " *"
+// }
+// console.log(bag);
+
+// for ( let i=1; i<=4; i++){
+//       bag = ""
+//       for ( let j=1; j<=i; j++){
+//           bag += "*" + " "
+//       }
+//       console.log(bag);
+      
+//   }
+
+//   for ( let i=1; i<=5; i++){
+//     bag = ""
+//     bag += "*" + " "
+//   }
+// console.log(bag);
+
+
+// for ( let i=1; i<=5; i++){
+//   let bag = ''
+//   bag += i
+  
+// } 
+// console.log(bag );
+
+// for ( let i=1; i<=5; i++){
+//   bag = ""
+//   bag += " * " + " "
+//   for ( let i=1; i<=5; i++){
+
+//   }
+// }
+// console.log(bag);
+
+
+// for ( let i=1; i<=5; i++){
+//       let bag = " ";
+  
+//       for ( let i=1; i<=5; i++){
+//           bag += "$" + " "
+//       }
+//       console.log(bag);
+//   } 
+
+
+// for ( let i=1; i<=5; i++){
+//   bag = ""
+//   for ( let j=1; j<=5; j++){
+//     bag += "*" + " "
+//   }
+//   console.log(bag);
+  
+// }
+
+
+// for ( let j=1; j<=5; j++){
+//   bag = ""
+//   for ( let i=1; i<=j; i++){
+//     bag += " * " + " "
+
+//   }
+//   console.log(bag);
+  
+// }
+// for ( let j=5; j>=0; j--){
+//   bag = ""
+//   for ( let i=1; i<=j; i++){
+//     bag += " * " + " "
+
+//   }
+//   console.log(bag);
+  
+// }
+
+
+for ( let i=1; i<=5; i+=){
+  bag = " "
+}
+
+
