0cffd3deabf7a9e85e7dadafddb7f05c9b86aab0
Aditya Kumar <adityadreamer9060@gmail.com>
2025-03-07 19:23:15 +0530

    Object complete

---
+++ Object/script.js
@@ -0,0 +1,131 @@
+console.log("Roadmap to 1 cr");
+
+// let obj = {}
+// console.log(obj);
+
+// let obj = {
+//     name : "Aditya",
+//     city : "gaya",
+//     age : 25
+// }
+// obj.salary = 25000;
+// obj.city = "Patna"
+// console.log(obj);
+// console.log(obj.name);
+// console.log(obj['city']);
+// delete obj ["city"] ;
+// console.log(obj);
+
+
+// let mydetails = {
+//     name : 'Aditya',
+    
+// }
+// mydetails.age = 25;
+// mydetails.job = "Frontend Developer";
+// mydetails.salary = 25000 ;
+// mydetails.city = " gaya"
+// mydetails.city = ' patna'
+// mydetails.marks ={
+//     eng : 30,
+//     hindi : 50,
+//     math : {
+//         py: 20,
+//         mt: 30
+//     }
+// }
+// mydetails.result= [10,20,30,40,50]
+// delete mydetails.job
+// console.log(mydetails.marks.math.py);
+// console.log(mydetails.result[4]);
+// console.log(mydetails);
+
+// for ( key in mydetails){
+//     console.log(mydetails[key]);
+    
+// }
+
+// for ( let a in mydetails){
+//     console.log(a,  '-' , mydetails[a]);
+    
+// }
+
+// let arr = [1,1,2,2,3,3,4,4,5,5];
+
+// let obj = {};
+
+// for ( let i=0; i<=arr.length-1; i++){
+//     if (obj[arr[i]] == undefined){
+//         obj[arr[i]] = 1
+//     }
+//     else{
+//         obj[arr[i]] += 1
+//     }
+// }
+// console.log(obj);
+
+
+
+
+
+// let arr = [5,5,6,6,7,7,8,8,8,9,9,9,9,9];
+
+// let obj = {};
+
+// for ( let i=0; i<=arr.length-1; i++){
+//     if ( obj[arr[i]] == undefined){
+//         obj[arr[i]] = 1
+//     }
+//     else{
+//         obj[arr[i]] += 1
+//     }
+// }
+// console.log(obj);
+
+
+// let obj = [
+//     {name : 'Aditya', age : 25 ,},
+//     {name : 'Adit', age : 26 ,},
+//     {name : 'Adiii', age : 27 ,}
+// ]
+// console.log(obj[1].name);
+
+
+// let obj = [
+//     {name: 'Aditya', age: 25},
+//     {name: 'Adit', age: 26},
+//     {name: 'Adiii', age: 27},
+// ]
+// console.log(obj[0].age);
+
+
+// myname = function(){
+//     console.log('Aditya');
+     
+// }
+// myname()
+
+// let obj = {
+//     name : 'Aditya',
+//     age : 25,
+//     Myname : function(){
+//         console.log('Adii');
+        
+//     }
+// }
+// console.log(obj.Myname());
+
+
+// let arr = [1,1,2,2,3,3,4,4,4,4,4,5,5,5];
+
+// let obj = {} ;
+
+// for ( let i=0; i<=arr.length-1; i++){
+//     if ( obj[arr[i]] == undefined){
+//         obj[arr[i]] = 1
+//     }
+//     else {
+//         obj[arr[i]] += 1
+//     }
+// }
+// console.log(obj);
