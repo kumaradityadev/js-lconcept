0608b3383d9cafbcb4d59b3314d08f649cfc1264
Aditya Kumar <adityadreamer9060@gmail.com>
2025-03-14 18:13:00 +0530

    Dom another lec

---
+++ DOM-1/script.js
@@ -0,0 +1,68 @@
+// let ras = document.getElementById("ras")
+// console.log(ras);
+
+// let adii = document.getElementsByClassName("akd")
+// console.log(adii);
+
+// let para = document.getElementsByTagName("p")
+// console.log(para);
+
+// let box = document.querySelector("#ras")
+// console.log(box);
+
+// let box1 = document.querySelectorAll(".akd")
+// console.log(box1);
+
+// let box2 = document.querySelector(".akd")
+// console.log(box2.tagName);
+
+// let fru = document.querySelector(".fruit") ;
+
+// // console.log(fru.innerHTML);
+// console.log(fru.innerText);
+
+// let fruit = document.querySelector(".fruit");
+// console.log(fruit.innerText);
+// console.log(fruit.innerHTML);
+
+
+// let change = document.querySelector("h2");
+// change.innerText = change.innerText + " From apna college"
+// console.log(change);
+// // console.log(change);
+
+// let box = document.querySelectorAll("div")
+// box[0].innerText = "Aditya";
+// console.log(box[0]);
+
+// box[1].innerText = "Aditya";
+// console.log(box[0]);
+
+// box[2].innerText = "Aditya";
+// console.log(box[0]);
+
+
+
+// let div = document.querySelector("div")
+// div.style.color = " red"
+// div.style.color = "yellow"
+
+// let heading = document.querySelector("h2")
+// heading.style.color = " red"
+// console.log(heading.innerText);
+
+let btn = document.createElement("button")
+btn.innerText = " Clickme" ;
+
+let cont = document.querySelector("#container")
+cont.append.btn
+console.log(btn);
+
+
+
+
+
+
+
+
+
