ff4675fd5cea92bd326453be2234222908d50ba0
Aditya Kumar <adityadreamer9060@gmail.com>
2025-03-15 14:56:46 +0530

    Event-loop part 1 complete

---
+++ Event-loop/script.js
@@ -0,0 +1,102 @@
+console.log("Roadmap to 1 cr in 2035");
+
+// let str = "Masai";
+// console.log(str[2]);
+
+
+// let arr = ['Adit','Aditya','Adiii']
+// arr.splice(1,1,"Aditya123")
+// console.log(arr);
+
+
+// let obj = {
+//     name1: 'Aditya',
+//     age : 26
+// }
+// console.log(obj);
+// obj.name1 = " Abhishek"
+// Object.freeze(obj)
+// obj.name1 = 'Himanshu'
+// console.log(obj);
+
+// var 
+// var a = 10 ;
+// a = 20 ;
+// var a = 30 ;
+// console.log(a);
+
+// let 
+// let a = 10;
+// a = 20;
+// let a = 30;
+// console.log(a);
+
+// const 
+// const a = 10;
+// a = 20 ;
+// console.log(a);
+
+// function say(){
+//     var name1 = 'Aditya12'
+//    if (true){
+//     var myname = " adit"
+//     let myage = 26 ;
+//     const salary = 30000;
+//     console.log(myage,salary,name1);
+    
+//    }
+//     console.log(name1,myname);
+    
+    
+// }
+// say()
+
+// console.log(c);
+// // console.log(a);
+// // console.log(b);
+
+// var c = 'Adiii'
+// let a = 'Aditya'
+// const b = 'Adit'
+
+// var name1 = ' Adit';
+
+// function mydetails(){
+//     var name2 = ' Adiii';
+//     let name3 = 'Aditya';
+
+//     console.log(name2);
+//     console.log(name3);
+    
+// }
+// console.log(name1);
+// mydetails()
+
+// function scopetest(){
+//     var outbar = 'outarbar';
+//     let iner = 'innertest';
+
+//     if (true){
+//         var name1 = 'Aditya';
+//         let name2 = 'Aditya123';
+
+//     }
+  
+//     console.log(name1);
+//     console.log(name2);
+// }
+// scopetest();
+
+
+// console.log(name);
+// var name = 'Aditya';
+
+// var c = 'Aditya'
+// console.log(c);
+
+function say(){
+    console.log(c);
+    
+}
+var c = 'Adit'
+say()
---
+++ Local-Storage/script.js
@@ -0,0 +1,99 @@
+console.log('Roadmap to 1 cr');
+
+// let myname = 'Aditya';
+// localStorage.setItem("myname" , myname)
+
+// let no = 55;
+// localStorage.setItem("number", no)
+
+
+// let school = "Masai School"
+// localStorage.setItem("school" ,school )
+
+// let grade = 75;
+// localStorage.setItem("grade" , grade)
+
+// let grades = localStorage.get 
+
+
+// let myname = 'Aditya' ;
+// localStorage.setItem("myname" , myname)
+
+// let num = 55;
+// localStorage.setItem("number" , num)
+
+// let nums = localStorage.getItem("number")
+// console.log(nums);
+
+// let name = localStorage.getItem("myname")
+// console.log(name);
+
+// // localStorage.removeItem("number");
+// // localStorage.removeItem("myname");
+
+
+// localStorage.clear();
+
+
+// json
+
+// let obj = {
+//     name : 'Aditya',
+//     age : 25
+// }
+
+// let jobj = {
+//     "name": "aditya",
+//     "age": 25
+// }
+
+
+// let myname = 'Aditya';
+
+// localStorage.setItem("myname", myname)
+
+
+// let no = 55;
+// localStorage.setItem("number" , no)
+
+// let num = localStorage.getItem("number")
+// console.log(num);
+
+// let mynames = localStorage.getItem("myname");
+// console.log(mynames);
+
+// localStorage.removeItem("myname");
+// localStorage.clear();
+
+// object store karna 
+// let obj = {
+//     name1: 'Aditya',
+//     age : 25
+// }
+// let objj = {
+//     "name1": 'Aditya',
+//    " age" : 25
+// }
+
+// localStorage.setItem("userobj" , obj.stringify )
+
+// localStorage.setItem("user" , JSON.stringify(obj)) 
+
+
+// localStorage.setItem("user", JSON.stringify(obj))
+
+
+// ?array store karna .
+
+// let arr = [ 1,2,3,4,5];
+// localStorage.setItem("array" ,JSON.stringify(arr))
+
+
+// let obj = {
+//     name1 : 'Aditya',
+//     age : 25
+// }
+// localStorage.setItem("user" ,JSON.stringify(obj))
+
+// let obj1 = JSON.parse(localStorage.getItem("user"))
+// console.log(obj1);
