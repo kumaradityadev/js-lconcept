b5c253320f98c611e2eae6927cdde3f9b24207c5
Aditya Kumar <adityadreamer9060@gmail.com>
2025-03-17 07:37:36 +0530

    Event loop 2 done

---
+++ Event-loop 2/script.js
@@ -0,0 +1,63 @@
+// console.log('Roadmap to 1 cr');
+// console.log('Aditya');
+
+// for(let i=0; i<=7000000000; i++){}
+
+// console.log('Abhishek');
+
+
+// setTimeout( () =>{
+//     console.log('Hello');
+    
+// },1000)
+// setTimeout( () =>{
+//     console.log('Hello');
+    
+// },3000)
+// setTimeout( () =>{
+//     console.log('Hello');
+    
+// },5000)
+// setTimeout( () =>{
+//     console.log('Hello');
+    
+// },7000)
+
+// console.log('Adit');
+
+// let akd =setTimeout( () =>{
+//     console.log('Hello');
+    
+// },2000)
+
+// console.log(akd);
+// clearTimeout(akd)
+
+// console.log('Aditya'); 
+
+
+// setInterval( () =>{
+//     console.log('Aditya');
+    
+// },3000);
+
+// function say(){
+//     console.log('Hello Aditya');
+    
+// }
+// setTimeout(say , 3000);
+// setInterval(say , 3000);
+
+
+count = 0;
+
+// setInterval( () =>{
+//     count++ ;
+//     console.log(count);
+    
+// },2000)
+
+console.log('a');
+console.log('a');
+console.log('a');
+console.log('a');
